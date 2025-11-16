827cde4680f5bcd5607dcb7275c5b84d806a4e0c
Jarek Lin <s410673074@gmail.com>
2025-11-16 12:30:13 +0800

    setup

---
+++ src/nautilus-server/src/lib.rs
@@ -0,0 +1,58 @@
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::response::Response;
+use axum::Json;
+use fastcrypto::ed25519::Ed25519KeyPair;
+use serde_json::json;
+use std::fmt;
+
+mod apps {
+    #[path = "zing-watermark/mod.rs"]
+    pub mod zing_watermark;
+}
+
+pub mod app {
+    pub use crate::apps::zing_watermark::*;
+}
+
+pub mod common;
+
+/// App state, at minimum needs to maintain the ephemeral keypair.  
+pub struct AppState {
+    /// Ephemeral keypair on boot
+    pub eph_kp: Ed25519KeyPair,
+    /// API key when querying api.weatherapi.com
+    pub file_key: String,
+}
+
+/// Implement IntoResponse for EnclaveError.
+impl IntoResponse for EnclaveError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            EnclaveError::GenericError(e) => (StatusCode::BAD_REQUEST, e),
+        };
+        let body = Json(json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
+    }
+}
+
+/// Enclave errors enum.
+#[derive(Debug)]
+pub enum EnclaveError {
+    GenericError(String),
+}
+
+impl fmt::Display for EnclaveError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EnclaveError::GenericError(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl std::error::Error for EnclaveError {}
---
+++ src/nautilus-server/src/main.rs
@@ -0,0 +1,44 @@
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use axum::{routing::get, routing::post, Router};
+use fastcrypto::{ed25519::Ed25519KeyPair, traits::KeyPair};
+use nautilus_server::app::process_data;
+use nautilus_server::common::{get_attestation, health_check};
+use nautilus_server::AppState;
+use std::sync::Arc;
+use tower_http::cors::{Any, CorsLayer};
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // we assign each Enclave an keypair when setup
+    let eph_kp = Ed25519KeyPair::generate(&mut rand::thread_rng());
+    let file_key = String::new();
+
+    let state = Arc::new(AppState { eph_kp, file_key });
+
+    nautilus_server::app::spawn_host_init_server(state.clone()).await?;
+
+    // Define your own restricted CORS policy here if needed.
+    let cors = CorsLayer::new().allow_methods(Any).allow_headers(Any);
+
+    let app = Router::new()
+        .route("/", get(ping))
+        .route("/get_attestation", get(get_attestation))
+        .route("/process_data", post(process_data)) // exposed to client
+        .route("/health_check", get(health_check))
+        .with_state(state)
+        .layer(cors);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    info!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app.into_make_service())
+        .await
+        .map_err(|e| anyhow::anyhow!("Server error: {}", e))
+}
+
+async fn ping() -> &'static str {
+    "Pong!"
+}
